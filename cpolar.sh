4641c510f6e37e3fbb76076ebe17de73b71fc629
gameloader <55951833+game-loader@users.noreply.github.com>
2022-03-17 11:07:23 +0800

    Add files via upload

--- cpolar.sh
+++ cpolar.sh
@@ -1,4 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/bash
+pacakges=("wget" "unzip")
+if [ "$(whoami)" = "root" ]
+then
+    user="sudo"
+else
+    user=""
+fi
+
+for pack in "${pacakges[@]}"
+do
+    if [ $( ${user} apt list --installed | grep "${pack}") -eq 0 ]
+    then
+        apt-get install "${pack}"
+    fi
+done
 
 wget https://www.cpolar.com/static/downloads/releases/latest/cpolar-stable-linux-amd64.zip
 unzip ./cpolar-stable-linux-amd64.zip
